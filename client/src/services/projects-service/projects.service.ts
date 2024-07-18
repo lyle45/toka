63012cbab07128bc21fa7bdad458795c85bd925c
Lyle <lylelamed45@gmail.com>
2024-07-18 05:14:01 +0300

    feat(main): Added projects service

---
+++ client/src/services/projects-service/projects.service.ts
@@ -0,0 +1,31 @@
+import { httpService } from '@/services/http-service/http.service';
+import type { Project } from '@/models/project.model';
+
+class ProjectsService {
+  public slug = 'projects';
+
+  public async getProjects() {
+    const response = await httpService.get<Project[]>(this.slug);
+    return response.data;
+  }
+
+  public async getProject(projectId: string) {
+    const response = await httpService.get<Project>(`${this.slug}/${projectId}`);
+    return response.data;
+  }
+
+  public async createProject(project: Omit<Project, '_id'>) {
+    const response = await httpService.post<Project>(this.slug, project);
+    return response.data;
+  }
+
+  public async updateProject(project: Project) {
+    const response = await httpService.put<Project>(`${this.slug}/${project._id}`, project);
+    return response.data;
+  }
+
+  public async deleteProject(projectId: string) {
+    const response = await httpService.delete<{ id: string }>(`${this.slug}/${projectId}`);
+    return response.data;
+  }
+}
