65ff1b3ec7645b183073851022671727a07e9524
lylelamed <lylelamed45@gmail.com>
2024-07-19 09:51:21 +0300

    feat(main): Add a hover effect to project items

--- client/src/components/ProjectsList/ProjectItem/ProjectItem.vue
+++ client/src/components/ProjectsList/ProjectItem/ProjectItem.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="project-item">
     <template v-if="loading">
-      <Skeletor width="60%" height="24px" pill class="project-title" />
-      <Skeletor width="80%" height="18px" pill class="project-description" />
-      <Skeletor width="40%" height="18px" pill class="project-date" />
+      <Skeletor class="project-title" width="60%" height="24px" pill />
+      <Skeletor class="project-description" width="80%" height="18px" pill />
+      <Skeletor class="project-date" width="40%" height="18px" pill />
     </template>
     <template v-else-if="project">
       <h3 class="project-title">{{ project.name }}</h3>
@@ -41,9 +41,22 @@ const formattedDate = computed(() => {
 .project-item {
   padding: 16px;
   background-color: #fff;
+  overflow: hidden;
+  cursor: pointer;
   box-shadow:
     0 4px 6px -1px rgba(0, 0, 0, 0.1),
     0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  transition:
+    background-color 0.2s,
+    border 0.2s;
+  border-radius: 8px;
+  border: 1px solid transparent;
+}
+
+.project-item:hover {
+  box-shadow: 0 8px 16px -8px rgba(0, 0, 0, 0.2);
+  background-color: #f0f0f0; /* Light grey background color on hover */
+  border: 1px solid #ccc; /* Subtle border on hover */
 }
 
 .project-title {
